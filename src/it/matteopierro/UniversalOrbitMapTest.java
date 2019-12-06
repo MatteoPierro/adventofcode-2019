7662baa89e81772159b6a0da91e10f317c7eb30a
Matteo Pierro <pierromatteo@gmail.com>
2019-12-06 08:29:23 +0100

    solution first challenge day6

--- src/it/matteopierro/UniversalOrbitMapTest.java
+++ src/it/matteopierro/UniversalOrbitMapTest.java
@@ -2,11 +2,15 @@ package it.matteopierro;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import static java.util.Arrays.asList;
 import static java.util.Collections.unmodifiableMap;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -14,14 +18,14 @@ class UniversalOrbitMapTest {
 
     @Test
     void findAnOrbit() {
-        Universe universe = new Universe(new String[]{"COM)B", "A)B"});
+        Universe universe = new Universe(asList("COM)B", "A)B"));
 
         assertThat(universe.directOrbits()).hasSize(2);
     }
 
     @Test
     void findDirectAndIndirectOrbitsForAPlanet() {
-        Universe universe = new Universe(new String[]{"COM)B", "B)C"});
+        Universe universe = new Universe(asList("COM)B", "B)C"));
 
         assertThat(universe.orbitsFor("C"))
                 .containsExactlyInAnyOrder("COM)C", "B)C");
@@ -30,11 +34,19 @@ class UniversalOrbitMapTest {
     @Test
     void findTotalOrbits() {
         String[] orbits = {"COM)B", "B)C", "C)D", "D)E", "E)F", "B)G", "G)H", "D)I", "E)J", "J)K", "K)L"};
-        Universe universe = new Universe(orbits);
+        Universe universe = new Universe(asList(orbits));
 
         assertThat(universe.orbitsFor("D")).hasSize(3);
         assertThat(universe.orbitsFor("L")).hasSize(7);
         assertThat(universe.totalOrbits()).hasSize(42);
+    }
+
+    @Test
+    void firstPuzzle() throws IOException {
+        List<String> orbits = Files.readAllLines(Paths.get("./input_day6"));
+        Universe universe = new Universe(orbits);
+
+        assertThat(universe.totalOrbits()).hasSize(402879);
     }
 
     public static class Universe {
@@ -42,7 +54,7 @@ class UniversalOrbitMapTest {
         private final Map<String, String> reverseOrbits;
 
 
-        Universe(String[] orbits) {
+        Universe(List<String> orbits) {
             this.orbits = new HashMap<>();
             this.reverseOrbits = new HashMap<>();
             for (String relation : orbits) {
