c0e96b0f99d25abd3025d2bee2f2bebb71868e8f
Matteo Pierro <pierromatteo@gmail.com>
2019-12-07 07:33:38 +0100

    extract computer

--- src/it/matteopierro/ChanceOfAsteroidsTest.java
+++ src/it/matteopierro/ChanceOfAsteroidsTest.java
@@ -1,5 +1,6 @@
 package it.matteopierro;
 
+import it.matteopierro.computer.Computer;
 import org.jooq.lambda.Seq;
 import org.jooq.lambda.tuple.Tuple2;
 import org.junit.jupiter.api.Test;
@@ -9,23 +10,13 @@ import org.junit.jupiter.params.provider.CsvSource;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class ChanceOfAsteroidsTest {
-    private static final String SUM_OPERATION = "1";
-    private static final String MULTIPLY_OPERATION = "2";
-    private static final String SAVE_OPERATION = "3";
-    private static final String STOP_OPERATION = "99";
-    private static final String READ_OPERATION = "4";
-    private static final String JUMP_IF_TRUE = "5";
-    private static final String JUMP_IF_FALSE = "6";
-    private static final String LESS_OPERATION = "7";
-    private static final String EQUAL_OPERATION = "8";
-
     private static final String INPUT = "1";
+    private final Computer computer = new Computer();
 
     @ParameterizedTest
     @CsvSource(value = {
@@ -66,7 +57,7 @@ class ChanceOfAsteroidsTest {
     @Test
     void thirdPuzzle() throws IOException {
         String[] program = Files.readString(Paths.get("./input_day5")).split(",");
-        List<String> results = execute(program, "1");
+        List<String> results = computer.execute(program, "1");
         assertThat(results).containsExactly("0", "0", "0", "0", "0", "0", "0", "0", "0", "12234644");
     }
 
@@ -77,7 +68,7 @@ class ChanceOfAsteroidsTest {
     })
     void jumpPositionMode(String input, String expectedResult) {
         String program = "3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9";
-        List<String> result = execute(input, program);
+        List<String> result = computer.execute(input, program);
         assertThat(result).containsExactly(expectedResult);
     }
 
@@ -88,7 +79,7 @@ class ChanceOfAsteroidsTest {
     })
     void jumpImmediateMode(String input, String expectedResult) {
         String program = "3,3,1105,-1,9,1101,0,0,12,4,12,99,1";
-        List<String> result = execute(input, program);
+        List<String> result = computer.execute(input, program);
         assertThat(result).containsExactly(expectedResult);
     }
 
@@ -99,7 +90,7 @@ class ChanceOfAsteroidsTest {
     })
     void equalPositionMode(String input, String expectedResult) {
         String program = "3,9,8,9,10,9,4,9,99,-1,8";
-        List<String> result = execute(input, program);
+        List<String> result = computer.execute(input, program);
         assertThat(result).containsExactly(expectedResult);
     }
 
@@ -110,7 +101,7 @@ class ChanceOfAsteroidsTest {
     })
     void equalImmediateMode(String input, String expectedResult) {
         String program = "3,3,1108,-1,8,3,4,3,99";
-        List<String> result = execute(input, program);
+        List<String> result = computer.execute(input, program);
         assertThat(result).containsExactly(expectedResult);
     }
 
@@ -121,7 +112,7 @@ class ChanceOfAsteroidsTest {
     })
     void lessPositionMode(String input, String expectedResult) {
         String program = "3,9,7,9,10,9,4,9,99,-1,8";
-        List<String> result = execute(input, program);
+        List<String> result = computer.execute(input, program);
         assertThat(result).containsExactly(expectedResult);
     }
 
@@ -132,7 +123,7 @@ class ChanceOfAsteroidsTest {
     })
     void lessImmediateMode(String input, String expectedResult) {
         String program = "3,3,1107,-1,8,3,4,3,99";
-        List<String> result = execute(input, program);
+        List<String> result = computer.execute(input, program);
         assertThat(result).containsExactly(expectedResult);
     }
 
@@ -146,20 +137,20 @@ class ChanceOfAsteroidsTest {
         String program = "3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31," +
                 "1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104," +
                 "999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99";
-        List<String> result = execute(input, program);
+        List<String> result = computer.execute(input, program);
         assertThat(result).containsExactly(expectedResult);
     }
 
     @Test
     void forthPuzzle() throws IOException {
         String[] program = Files.readString(Paths.get("./input_day5")).split(",");
-        List<String> results = execute(program, "5");
+        List<String> results = computer.execute(program, "5");
         assertThat(results).containsExactly("3508186");
     }
 
     @Test
     void outputItsInput() {
-        List<String> output = execute(INPUT, "3,0,4,0,99");
+        List<String> output = computer.execute(INPUT, "3,0,4,0,99");
         assertThat(output).containsExactly(INPUT);
     }
 
@@ -180,278 +171,13 @@ class ChanceOfAsteroidsTest {
     }
 
     private String compute(String[] instructions) {
-        execute(instructions, INPUT);
+        computer.execute(instructions, INPUT);
         return instructions[0];
     }
 
     String execute(String program) {
         String[] instructions = program.split(",");
-        execute(instructions, INPUT);
+        computer.execute(instructions, INPUT);
         return String.join(",", instructions);
-    }
-
-    private List<String> execute(String input, String program) {
-        return execute(program.split(","), input);
-    }
-
-    private List<String> execute(String[] memory, String input) {
-        List<String> outputs = new ArrayList<>();
-        for (int memoryIndex = 0; memoryIndex < memory.length; ) {
-            String operationCode = memory[memoryIndex];
-            Operation operation = operationFor(operationCode, outputs, input);
-            memoryIndex = operation.execute(memory, memoryIndex);
-        }
-        return outputs;
-    }
-
-    private Operation operationFor(String operationCode, List<String> outputs, String input) {
-        if (STOP_OPERATION.equals(operationCode)) {
-            return new Stop();
-        } else if (operationCode.endsWith(SUM_OPERATION)) {
-            return new Sum(operationCode);
-        } else if (operationCode.endsWith(MULTIPLY_OPERATION)) {
-            return new Multiply(operationCode);
-        } else if (operationCode.endsWith(SAVE_OPERATION)) {
-            return new Save(input);
-        } else if (operationCode.endsWith(READ_OPERATION)) {
-            return new Read(operationCode, outputs);
-        } else if (operationCode.endsWith(JUMP_IF_TRUE)) {
-            return new JumpIfTrue(operationCode);
-        } else if (operationCode.endsWith(JUMP_IF_FALSE)) {
-            return new JumpIfFalse(operationCode);
-        } else if (operationCode.endsWith(LESS_OPERATION)) {
-            return new Less(operationCode);
-        } else if (operationCode.endsWith(EQUAL_OPERATION)) {
-            return new Equal(operationCode);
-        }
-        throw new RuntimeException("Not Supported Operation! " + operationCode);
-    }
-
-    private interface Mode {
-        static Mode modeFor(String mode) {
-            return mode.equals("0")
-                    ? new Position()
-                    : new Immediate();
-        }
-
-        Integer read(String[] memory, Integer address);
-
-        void write(String[] memory, Integer address, String value);
-    }
-
-    private static class Position implements Mode {
-        @Override
-        public Integer read(String[] memory, Integer address) {
-            int position = Integer.parseInt(memory[address]);
-            return Integer.parseInt(memory[position]);
-        }
-
-        @Override
-        public void write(String[] memory, Integer address, String value) {
-            int position = Integer.parseInt(memory[address]);
-            memory[position] = String.valueOf(value);
-        }
-    }
-
-    private static class Immediate implements Mode {
-        @Override
-        public Integer read(String[] memory, Integer address) {
-            return Integer.parseInt(memory[address]);
-        }
-
-        @Override
-        public void write(String[] memory, Integer address, String value) {
-            memory[address] = String.valueOf(value);
-        }
-    }
-
-    private interface Operation {
-        int execute(String[] memory, int memoryIndex);
-    }
-
-    public static abstract class OneOperandOperation implements Operation {
-        private final Mode firstOperandMode;
-
-        OneOperandOperation(String operationCode) {
-            this.firstOperandMode = extractFirstOperandMode(operationCode);
-        }
-
-        private Mode extractFirstOperandMode(String operationCode) {
-            if (operationCode.length() < 3) return new Position();
-
-            return Mode.modeFor(operationCode.split("")[operationCode.length() - 3]);
-        }
-
-        @Override
-        public int execute(String[] memory, int memoryIndex) {
-            Integer firstOperand = firstOperandMode.read(memory, memoryIndex + 1);
-            return execute(memory, memoryIndex, firstOperand);
-        }
-
-        protected abstract int execute(String[] memory, int memoryIndex, Integer firstOperand);
-    }
-
-    public static abstract class TwoOperandOperation extends OneOperandOperation {
-        private final Mode secondOperandMode;
-
-        TwoOperandOperation(String operationCode) {
-            super(operationCode);
-            this.secondOperandMode = extractSecondOperandMode(operationCode);
-        }
-
-        private Mode extractSecondOperandMode(String operationCode) {
-            if (operationCode.length() < 4) return new Position();
-
-            return Mode.modeFor(operationCode.split("")[operationCode.length() - 4]);
-        }
-
-        @Override
-        public int execute(String[] memory, int memoryIndex, Integer firstOperand) {
-            Integer secondOperand = secondOperandMode.read(memory, memoryIndex + 2);
-            return execute(memory, memoryIndex, firstOperand, secondOperand);
-        }
-
-        protected abstract int execute(String[] memory, int memoryIndex, Integer firstOperand, Integer secondOperand);
-    }
-
-    public static abstract class TwoOperandWithResult extends TwoOperandOperation {
-        private static final int INSTRUCTION_SIZE = 4;
-        private final Mode resultMode;
-
-        TwoOperandWithResult(String operationCode) {
-            super(operationCode);
-            this.resultMode = new Position();
-        }
-
-        @Override
-        protected int execute(String[] memory, int memoryIndex, Integer firstOperand, Integer secondOperand) {
-            String value = String.valueOf(execute(firstOperand, secondOperand));
-            resultMode.write(memory, memoryIndex + 3, value);
-            return memoryIndex + INSTRUCTION_SIZE;
-        }
-
-        protected abstract int execute(int firstOperand, int secondOperand);
-    }
-
-    private static class Multiply extends TwoOperandWithResult {
-
-        Multiply(String operationCode) {
-            super(operationCode);
-        }
-
-        @Override
-        protected int execute(int firstOperand, int secondOperand) {
-            return firstOperand * secondOperand;
-        }
-    }
-
-    private static class Sum extends TwoOperandWithResult {
-        Sum(String operationCode) {
-            super(operationCode);
-        }
-
-        @Override
-        protected int execute(int firstOperand, int secondOperand) {
-            return firstOperand + secondOperand;
-        }
-    }
-
-    private static class Save implements Operation {
-
-        private final String input;
-
-        Save(String input) {
-            this.input = input;
-        }
-
-        @Override
-        public int execute(String[] memory, int memoryIndex) {
-            int savePosition = Integer.parseInt(memory[memoryIndex + 1]);
-            memory[savePosition] = input;
-            return memoryIndex + 2;
-        }
-    }
-
-    private static class Read extends OneOperandOperation {
-        private final List<String> outputs;
-
-        Read(String operationCode, List<String> outputs) {
-            super(operationCode);
-            this.outputs = outputs;
-        }
-
-        @Override
-        protected int execute(String[] memory, int memoryIndex, Integer firstOperand) {
-            outputs.add(String.valueOf(firstOperand));
-            return memoryIndex + 2;
-        }
-    }
-
-    private static class Stop implements Operation {
-        @Override
-        public int execute(String[] memory, int memoryIndex) {
-            return memory.length;
-        }
-    }
-
-    public static abstract class Jump extends TwoOperandOperation {
-        Jump(String operationCode) {
-            super(operationCode);
-        }
-
-        @Override
-        protected int execute(String[] memory, int memoryIndex, Integer firstOperand, Integer secondOperand) {
-            if (jumpCondition(firstOperand)) return secondOperand;
-
-            return memoryIndex + 3;
-        }
-
-        protected abstract boolean jumpCondition(Integer firstOperand);
-    }
-
-    private static class JumpIfTrue extends Jump {
-        JumpIfTrue(String operationCode) {
-            super(operationCode);
-        }
-
-        @Override
-        protected boolean jumpCondition(Integer firstOperand) {
-            return firstOperand != 0;
-        }
-    }
-
-    private static class JumpIfFalse extends Jump {
-        JumpIfFalse(String operationCode) {
-            super(operationCode);
-        }
-
-        @Override
-        protected boolean jumpCondition(Integer firstOperand) {
-            return firstOperand == 0;
-        }
-    }
-
-    private static class Less extends TwoOperandWithResult {
-
-        Less(String operationCode) {
-            super(operationCode);
-        }
-
-        @Override
-        protected int execute(int firstOperand, int secondOperand) {
-            return firstOperand < secondOperand ? 1 : 0;
-        }
-    }
-
-    private static class Equal extends TwoOperandWithResult {
-
-        Equal(String operationCode) {
-            super(operationCode);
-        }
-
-        @Override
-        protected int execute(int firstOperand, int secondOperand) {
-            return firstOperand == secondOperand ? 1 : 0;
-        }
     }
 }
---
+++ src/it/matteopierro/computer/Computer.java
@@ -0,0 +1,281 @@
+package it.matteopierro.computer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Computer {
+    private static final String SUM_OPERATION = "1";
+    private static final String MULTIPLY_OPERATION = "2";
+    private static final String SAVE_OPERATION = "3";
+    private static final String STOP_OPERATION = "99";
+    private static final String READ_OPERATION = "4";
+    private static final String JUMP_IF_TRUE = "5";
+    private static final String JUMP_IF_FALSE = "6";
+    private static final String LESS_OPERATION = "7";
+    private static final String EQUAL_OPERATION = "8";
+
+    public List<String> execute(String input, String program) {
+        return execute(program.split(","), input);
+    }
+
+    public List<String> execute(String[] memory, String input) {
+        List<String> outputs = new ArrayList<String>();
+        for (int memoryIndex = 0; memoryIndex < memory.length; ) {
+            String operationCode = memory[memoryIndex];
+            Operation operation = operationFor(operationCode, outputs, input);
+            memoryIndex = operation.execute(memory, memoryIndex);
+        }
+        return outputs;
+    }
+
+    private Operation operationFor(String operationCode, List<String> outputs, String input) {
+        if (STOP_OPERATION.equals(operationCode)) {
+            return new Stop();
+        } else if (operationCode.endsWith(SUM_OPERATION)) {
+            return new Sum(operationCode);
+        } else if (operationCode.endsWith(MULTIPLY_OPERATION)) {
+            return new Multiply(operationCode);
+        } else if (operationCode.endsWith(SAVE_OPERATION)) {
+            return new Save(input);
+        } else if (operationCode.endsWith(READ_OPERATION)) {
+            return new Read(operationCode, outputs);
+        } else if (operationCode.endsWith(JUMP_IF_TRUE)) {
+            return new JumpIfTrue(operationCode);
+        } else if (operationCode.endsWith(JUMP_IF_FALSE)) {
+            return new JumpIfFalse(operationCode);
+        } else if (operationCode.endsWith(LESS_OPERATION)) {
+            return new Less(operationCode);
+        } else if (operationCode.endsWith(EQUAL_OPERATION)) {
+            return new Equal(operationCode);
+        }
+        throw new RuntimeException("Not Supported Operation! " + operationCode);
+    }
+
+    private interface Mode {
+        static Mode modeFor(String mode) {
+            return mode.equals("0")
+                    ? new Position()
+                    : new Immediate();
+        }
+
+        Integer read(String[] memory, Integer address);
+
+        void write(String[] memory, Integer address, String value);
+    }
+
+    private static class Position implements Mode {
+        @Override
+        public Integer read(String[] memory, Integer address) {
+            int position = Integer.parseInt(memory[address]);
+            return Integer.parseInt(memory[position]);
+        }
+
+        @Override
+        public void write(String[] memory, Integer address, String value) {
+            int position = Integer.parseInt(memory[address]);
+            memory[position] = String.valueOf(value);
+        }
+    }
+
+    private static class Immediate implements Mode {
+        @Override
+        public Integer read(String[] memory, Integer address) {
+            return Integer.parseInt(memory[address]);
+        }
+
+        @Override
+        public void write(String[] memory, Integer address, String value) {
+            memory[address] = String.valueOf(value);
+        }
+    }
+
+    private interface Operation {
+        int execute(String[] memory, int memoryIndex);
+    }
+
+    public static abstract class OneOperandOperation implements Operation {
+        private final Mode firstOperandMode;
+
+        OneOperandOperation(String operationCode) {
+            this.firstOperandMode = extractFirstOperandMode(operationCode);
+        }
+
+        private Mode extractFirstOperandMode(String operationCode) {
+            if (operationCode.length() < 3) return new Position();
+
+            return Mode.modeFor(operationCode.split("")[operationCode.length() - 3]);
+        }
+
+        @Override
+        public int execute(String[] memory, int memoryIndex) {
+            Integer firstOperand = firstOperandMode.read(memory, memoryIndex + 1);
+            return execute(memory, memoryIndex, firstOperand);
+        }
+
+        protected abstract int execute(String[] memory, int memoryIndex, Integer firstOperand);
+    }
+
+    public static abstract class TwoOperandOperation extends OneOperandOperation {
+        private final Mode secondOperandMode;
+
+        TwoOperandOperation(String operationCode) {
+            super(operationCode);
+            this.secondOperandMode = extractSecondOperandMode(operationCode);
+        }
+
+        private Mode extractSecondOperandMode(String operationCode) {
+            if (operationCode.length() < 4) return new Position();
+
+            return Mode.modeFor(operationCode.split("")[operationCode.length() - 4]);
+        }
+
+        @Override
+        public int execute(String[] memory, int memoryIndex, Integer firstOperand) {
+            Integer secondOperand = secondOperandMode.read(memory, memoryIndex + 2);
+            return execute(memory, memoryIndex, firstOperand, secondOperand);
+        }
+
+        protected abstract int execute(String[] memory, int memoryIndex, Integer firstOperand, Integer secondOperand);
+    }
+
+    public static abstract class TwoOperandWithResult extends TwoOperandOperation {
+        private static final int INSTRUCTION_SIZE = 4;
+        private final Mode resultMode;
+
+        TwoOperandWithResult(String operationCode) {
+            super(operationCode);
+            this.resultMode = new Position();
+        }
+
+        @Override
+        protected int execute(String[] memory, int memoryIndex, Integer firstOperand, Integer secondOperand) {
+            String value = String.valueOf(execute(firstOperand, secondOperand));
+            resultMode.write(memory, memoryIndex + 3, value);
+            return memoryIndex + INSTRUCTION_SIZE;
+        }
+
+        protected abstract int execute(int firstOperand, int secondOperand);
+    }
+
+    private static class Multiply extends TwoOperandWithResult {
+
+        Multiply(String operationCode) {
+            super(operationCode);
+        }
+
+        @Override
+        protected int execute(int firstOperand, int secondOperand) {
+            return firstOperand * secondOperand;
+        }
+    }
+
+    private static class Sum extends TwoOperandWithResult {
+        Sum(String operationCode) {
+            super(operationCode);
+        }
+
+        @Override
+        protected int execute(int firstOperand, int secondOperand) {
+            return firstOperand + secondOperand;
+        }
+    }
+
+    private static class Save implements Operation {
+
+        private final String input;
+
+        Save(String input) {
+            this.input = input;
+        }
+
+        @Override
+        public int execute(String[] memory, int memoryIndex) {
+            int savePosition = Integer.parseInt(memory[memoryIndex + 1]);
+            memory[savePosition] = input;
+            return memoryIndex + 2;
+        }
+    }
+
+    private static class Read extends OneOperandOperation {
+        private final List<String> outputs;
+
+        Read(String operationCode, List<String> outputs) {
+            super(operationCode);
+            this.outputs = outputs;
+        }
+
+        @Override
+        protected int execute(String[] memory, int memoryIndex, Integer firstOperand) {
+            outputs.add(String.valueOf(firstOperand));
+            return memoryIndex + 2;
+        }
+    }
+
+    private static class Stop implements Operation {
+        @Override
+        public int execute(String[] memory, int memoryIndex) {
+            return memory.length;
+        }
+    }
+
+    public static abstract class Jump extends TwoOperandOperation {
+        Jump(String operationCode) {
+            super(operationCode);
+        }
+
+        @Override
+        protected int execute(String[] memory, int memoryIndex, Integer firstOperand, Integer secondOperand) {
+            if (jumpCondition(firstOperand)) return secondOperand;
+
+            return memoryIndex + 3;
+        }
+
+        protected abstract boolean jumpCondition(Integer firstOperand);
+    }
+
+    private static class JumpIfTrue extends Jump {
+        JumpIfTrue(String operationCode) {
+            super(operationCode);
+        }
+
+        @Override
+        protected boolean jumpCondition(Integer firstOperand) {
+            return firstOperand != 0;
+        }
+    }
+
+    private static class JumpIfFalse extends Jump {
+        JumpIfFalse(String operationCode) {
+            super(operationCode);
+        }
+
+        @Override
+        protected boolean jumpCondition(Integer firstOperand) {
+            return firstOperand == 0;
+        }
+    }
+
+    private static class Less extends TwoOperandWithResult {
+
+        Less(String operationCode) {
+            super(operationCode);
+        }
+
+        @Override
+        protected int execute(int firstOperand, int secondOperand) {
+            return firstOperand < secondOperand ? 1 : 0;
+        }
+    }
+
+    private static class Equal extends TwoOperandWithResult {
+
+        Equal(String operationCode) {
+            super(operationCode);
+        }
+
+        @Override
+        protected int execute(int firstOperand, int secondOperand) {
+            return firstOperand == secondOperand ? 1 : 0;
+        }
+    }
+}
