4d70264451db6693a83992c5fa0a31313c7052a3
Matteo Pierro <pierromatteo@gmail.com>
2019-12-10 20:49:32 +0100

    second attempt of implement the line of sight

--- src/it/matteopierro/MonitoringStationTest.java
+++ src/it/matteopierro/MonitoringStationTest.java
@@ -3,11 +3,20 @@ package it.matteopierro;
 import org.jooq.lambda.tuple.Tuple2;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.HashSet;
+import java.util.Comparator;
 import java.util.List;
-import java.util.Set;
 
+import static java.lang.Math.max;
+import static java.lang.Math.min;
+import static java.math.BigInteger.ZERO;
+import static java.math.RoundingMode.HALF_UP;
+import static java.util.Arrays.asList;
+import static java.util.stream.Collectors.toList;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.jooq.lambda.tuple.Tuple.tuple;
 
@@ -71,78 +80,185 @@ class MonitoringStationTest {
     }
 
     @Test
-    void findVisibleAsteroidsForOneAsteroid() {
-        String map =
-                        ".#..#\n" +
-                        ".....\n" +
-                        "#####\n" +
-                        "....#\n" +
-                        "...##";
-
-        List<Tuple2<Integer, Integer>> asteroids = findAllAsteroids(map);
-
-        assertThat(visibleAsteroids(tuple(1, 0), asteroids)).containsExactlyInAnyOrder(
+    void asteroidsOnTheLine() {
+        List<Tuple2<Integer, Integer>> asteroids = List.of(
                              tuple(1, 0),                           tuple(4, 0),
-                tuple(0, 2), tuple(1, 2), tuple(2, 2), tuple(3, 2), tuple(4, 2)
-        );
-        assertThat(visibleAsteroids(tuple(0, 2), asteroids)).containsExactlyInAnyOrder(
-                            tuple(1, 0),                          tuple(4, 0),
-                tuple(0, 2),tuple(1, 2),
-                                                      tuple(4, 3),
-                                         tuple(3, 4), tuple(4, 4)
-        );
-        assertThat(visibleAsteroids(tuple(0, 2), asteroids).size()).isEqualTo(6);
-        assertThat(visibleAsteroids(tuple(2, 2), asteroids).size()).isEqualTo(7);
-    }
+                tuple(0, 2), tuple(1, 2), tuple(2, 2), tuple(3, 2), tuple(4, 2),
+                                                                    tuple(4, 3),
+                                                       tuple(3, 4), tuple(4, 4));
 
-    private Set<Tuple2<Integer, Integer>> visibleAsteroids(Tuple2<Integer, Integer> asteroid, List<Tuple2<Integer, Integer>> asteroids) {
-        Set<Tuple2<Integer, Integer>> visibleAsteroids = new HashSet<>();
-
-        for (int rowIndex = 0; rowIndex < 5; rowIndex++) {
-            for (int columnIndex = 0; columnIndex < 5; columnIndex++) {
-                Tuple2<Integer, Integer> coordinates = tuple(columnIndex, rowIndex);
-                if (asteroids.contains(coordinates) && isOnLineOfSight(asteroid, coordinates, asteroids)) {
-                    visibleAsteroids.add(coordinates);
-                }
-            }
-        }
-
-        return visibleAsteroids;
+        assertThat(asteroidsOnTheLine(tuple(1,0), tuple(4,4), asteroids)).isEmpty();
+        assertThat(asteroidsOnTheLine(tuple(1,0), tuple(3,4), asteroids)).containsExactly(tuple(2, 2));
+        assertThat(asteroidsOnTheLine(tuple(1,0), tuple(2,2), asteroids)).isEmpty();
+        assertThat(asteroidsOnTheLine(tuple(0,2), tuple(1,2), asteroids)).isEmpty();
+        assertThat(asteroidsOnTheLine(tuple(0,2), tuple(4,2), asteroids)).containsExactly(tuple(1, 2), tuple(2, 2), tuple(3, 2));
+        assertThat(asteroidsOnTheLine(tuple(4,0), tuple(4,2), asteroids)).isEmpty();
+        assertThat(asteroidsOnTheLine(tuple(4,4), tuple(4,0), asteroids)).containsExactly(tuple(4,2), tuple(4,3));
     }
 
     @Test
     void lineOfSight() {
         List<Tuple2<Integer, Integer>> asteroids = List.of(
-                tuple(1, 0), tuple(4, 0),
+                             tuple(1, 0),                           tuple(4, 0),
                 tuple(0, 2), tuple(1, 2), tuple(2, 2), tuple(3, 2), tuple(4, 2),
-                tuple(4, 3),
-                tuple(3, 4), tuple(4, 4));
+                                                                    tuple(4, 3),
+                                                       tuple(3, 4), tuple(4, 4));
 
-        assertThat(isOnLineOfSight(tuple(1, 0), tuple(4, 3), asteroids)).isFalse();
-        assertThat(isOnLineOfSight(tuple(1, 0), tuple(4, 0), asteroids)).isTrue();
-        assertThat(isOnLineOfSight(tuple(1, 0), tuple(0, 2), asteroids)).isTrue();
+        assertThat(lineOfSight(tuple(1, 0), asteroids)).containsExactly(tuple(4, 0), tuple(0, 2), tuple(1, 2), tuple(2, 2), tuple(3, 2), tuple(4, 2), tuple(4, 4));
+        assertThat(lineOfSight(tuple(3, 2), asteroids)).containsExactly(tuple(1, 0), tuple(4, 0), tuple(2, 2), tuple(4, 2), tuple(4, 3), tuple(3, 4), tuple(4, 4));
+        assertThat(lineOfSight(tuple(3, 4), asteroids)).containsExactly(tuple(4, 0), tuple(0, 2), tuple(1, 2), tuple(2, 2), tuple(3, 2), tuple(4, 2), tuple(4, 3), tuple(4, 4));
     }
 
-    private boolean isOnLineOfSight(Tuple2<Integer, Integer> asteroid, Tuple2<Integer, Integer> target, List<Tuple2<Integer, Integer>> asteroids) {
-        if (target.v2.equals(asteroid.v2)) {
-            int max = Math.max(target.v1, asteroid.v1);
-            int min = Math.min(target.v1, asteroid.v1);
-            for (int columnIndex = max - 1; columnIndex > min; columnIndex--) {
-                if (asteroids.contains(tuple(columnIndex, asteroid.v2))) {
-                    return false;
-                }
-            }
-        }
-        if (target.v2 - asteroid.v2 > 0) {
-            int columnIndex = target.v1 - 1; //check if they are in the other direction
-            for (int rowIndex = target.v2 - 1; rowIndex > asteroid.v2; rowIndex--) {
-                if (asteroids.contains(tuple(columnIndex, rowIndex))) {
-                    return false;
-                }
-                columnIndex--;
-            }
-        }
-        return true;
+    @Test
+    void maxLineOfSight() {
+        List<Tuple2<Integer, Integer>> asteroids = List.of(
+                             tuple(1, 0),                           tuple(4, 0),
+                tuple(0, 2), tuple(1, 2), tuple(2, 2), tuple(3, 2), tuple(4, 2),
+                                                                    tuple(4, 3),
+                                                       tuple(3, 4), tuple(4, 4));
+
+        Tuple2<Tuple2<Integer, Integer>, Integer> result = maxLightOfSight(asteroids);
+        assertThat(asList(result.v1)).containsExactly(tuple(3,4));
+        assertThat(result.v2).isEqualTo(8);
+    }
+
+    @Test
+    void maxLineOfSightFirstExample() {
+        String space = "......#.#.\n" +
+                "#..#.#....\n" +
+                "..#######.\n" +
+                ".#.#.###..\n" +
+                ".#..#.....\n" +
+                "..#....#.#\n" +
+                "#..#....#.\n" +
+                ".##.#..###\n" +
+                "##...#..#.\n" +
+                ".#....####";
+        List<Tuple2<Integer, Integer>> asteroids = findAllAsteroids(space);
+        Tuple2<Tuple2<Integer, Integer>, Integer> result = maxLightOfSight(asteroids);
+        assertThat(asList(result.v1)).containsExactly(tuple(5,8));
+        assertThat(result.v2).isEqualTo(33);
+    }
+
+    @Test
+    void maxLineOfSightSecondExample() {
+        String space = ".#..##.###...#######\n" +
+                "##.############..##.\n" +
+                ".#.######.########.#\n" +
+                ".###.#######.####.#.\n" +
+                "#####.##.#.##.###.##\n" +
+                "..#####..#.#########\n" +
+                "####################\n" +
+                "#.####....###.#.#.##\n" +
+                "##.#################\n" +
+                "#####.##.###..####..\n" +
+                "..######..##.#######\n" +
+                "####.##.####...##..#\n" +
+                ".#####..#.######.###\n" +
+                "##...#.##########...\n" +
+                "#.##########.#######\n" +
+                ".####.#.###.###.#.##\n" +
+                "....##.##.###..#####\n" +
+                ".#.#.###########.###\n" +
+                "#.#.#.#####.####.###\n" +
+                "###.##.####.##.#..##";
+        List<Tuple2<Integer, Integer>> asteroids = findAllAsteroids(space);
+        Tuple2<Tuple2<Integer, Integer>, Integer> result = maxLightOfSight(asteroids);
+        assertThat(asList(result.v1)).containsExactly(tuple(11,13));
+        assertThat(result.v2).isEqualTo(210);
+    }
+
+    @Test
+    void maxLineOfSightThirdExample() {
+        String space = "#.#...#.#.\n" +
+                ".###....#.\n" +
+                ".#....#...\n" +
+                "##.#.#.#.#\n" +
+                "....#.#.#.\n" +
+                ".##..###.#\n" +
+                "..#...##..\n" +
+                "..##....##\n" +
+                "......#...\n" +
+                ".####.###.";
+        List<Tuple2<Integer, Integer>> asteroids = findAllAsteroids(space);
+        Tuple2<Tuple2<Integer, Integer>, Integer> result = maxLightOfSight(asteroids);
+        assertThat(asList(result.v1)).containsExactly(tuple(1,2));
+        assertThat(result.v2).isEqualTo(35);
+    }
+
+    @Test
+    void maxLineOfSightForthExample() {
+        String space = ".#..#..###\n" +
+                "####.###.#\n" +
+                "....###.#.\n" +
+                "..###.##.#\n" +
+                "##.##.#.#.\n" +
+                "....###..#\n" +
+                "..#.#..#.#\n" +
+                "#..#.#.###\n" +
+                ".##...##.#\n" +
+                ".....#.#..";
+        List<Tuple2<Integer, Integer>> asteroids = findAllAsteroids(space);
+        Tuple2<Tuple2<Integer, Integer>, Integer> result = maxLightOfSight(asteroids);
+        assertThat(asList(result.v1)).containsExactly(tuple(6,3));
+        assertThat(result.v2).isEqualTo(41);
+    }
+
+    @Test
+    void firstPuzzle() throws IOException {
+        String space = Files.readString(Paths.get("./input_day10"));
+
+        List<Tuple2<Integer, Integer>> asteroids = findAllAsteroids(space);
+        Tuple2<Tuple2<Integer, Integer>, Integer> result = maxLightOfSight(asteroids);
+        assertThat(asList(result.v1)).containsExactly(tuple(18,25));
+        assertThat(result.v2).isEqualTo(301);
+    }
+
+    private Tuple2<Tuple2<Integer, Integer>, Integer> maxLightOfSight(List<Tuple2<Integer, Integer>> asteroids) {
+        return asteroids.stream()
+                .map(asteroid -> tuple(asteroid, lineOfSight(asteroid, asteroids)))
+                .map(t -> tuple(t.v1, t.v2.size()))
+                .max(Comparator.comparingInt(t -> t.v2))
+                .orElseThrow();
+    }
+
+    private List<Tuple2<Integer, Integer>> lineOfSight(Tuple2<Integer,Integer> candidate, List<Tuple2<Integer, Integer>> asteroids) {
+        return asteroids.stream()
+                .map( target -> tuple(target, asteroidsOnTheLine(candidate, target, asteroids).size()))
+                .filter(t-> t.v2 == 0)
+                .map(t -> t.v1)
+                .filter(t -> !t.equals(candidate))
+                .collect(toList());
+    }
+
+    @Test
+    void testSlop() {
+        assertThat(slope(tuple(1,0), tuple(1,0))).isEqualTo(slope(tuple(4,0), tuple(1,0)));
+        assertThat(slope(tuple(1,0), tuple(4,0))).isEqualTo(slope(tuple(4,0), tuple(1,0)));
+        assertThat(slope(tuple(1,0), tuple(1,2))).isEqualTo(slope(tuple(1,2), tuple(1,0)));
+        assertThat(slope(tuple(1,0), tuple(0,2))).isEqualTo(slope(tuple(0,2), tuple(1,0)));
+        assertThat(slope(tuple(1,0), tuple(3,2))).isEqualTo(slope(tuple(4,3), tuple(1,0)));
+        assertThat(slope(tuple(1,0), tuple(2,2))).isEqualTo(slope(tuple(3,4), tuple(1,0)));
+    }
+
+    private List<Tuple2<Integer, Integer>> asteroidsOnTheLine(Tuple2<Integer,Integer> pointA, Tuple2<Integer,Integer> pointB, List<Tuple2<Integer, Integer>> asteroids) {
+        BigDecimal slope = slope(pointA, pointB);
+        return asteroids.stream()
+                .filter( asteroid -> !asteroid.equals(pointA))
+                .filter( asteroid -> !asteroid.equals(pointB))
+                .map( asteroid -> tuple(asteroid, slope(asteroid, pointA)))
+                .filter( t -> t.v2.equals(slope))
+                .map( t -> t.v1)
+                .filter( t -> t.v1 >= min(pointA.v1, pointB.v1))
+                .filter( t -> t.v1 <= max(pointA.v1, pointB.v1))
+                .filter( t -> t.v2 >= min(pointA.v2, pointB.v2))
+                .filter( t -> t.v2 <= max(pointA.v2, pointB.v2))
+                .collect(toList());
+    }
+
+    private BigDecimal slope(Tuple2<Integer, Integer> pointA, Tuple2<Integer, Integer> pointB) {
+        if (pointA.v1.equals(pointB.v1)) return new BigDecimal(ZERO, 2);
+        return new BigDecimal(pointB.v2 - pointA.v2).divide(new BigDecimal(pointB.v1 - pointA.v1), 2, HALF_UP);
     }
 
     private List<Tuple2<Integer, Integer>> findAllAsteroids(String map) {
