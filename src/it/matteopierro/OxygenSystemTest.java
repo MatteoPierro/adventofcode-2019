a53bf1407b79a48dab07dae08afbdd200cf52422
Matteo Pierro <pierromatteo@gmail.com>
2019-12-15 09:32:27 +0100

    introduce directions

--- src/it/matteopierro/OxygenSystemTest.java
+++ src/it/matteopierro/OxygenSystemTest.java
@@ -1,9 +1,12 @@
 package it.matteopierro;
 
 import it.matteopierro.computer.ComputerListener;
+import it.matteopierro.robot.Direction;
 import org.jooq.lambda.tuple.Tuple2;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.jooq.lambda.tuple.Tuple.tuple;
@@ -43,33 +46,42 @@ class OxygenSystemTest {
         droid.onStoreRequested(Droid.WALL);
         assertThat(droid.onReadRequested()).isEqualTo(Droid.EAST);
         droid.onStoreRequested(Droid.SUCCESS);
-        assertThat((Iterable<?>) droid.currentPosition()).isEqualTo(tuple(1, 0));
+        assertThat((Iterable<?>) droid.currentPosition()).isEqualTo(tuple(-1, 0));
     }
 
-    private class Droid extends ComputerListener {
+    private static class Droid extends ComputerListener {
+
+        private static final String NORTH = "1";
+        private static final String SOUTH = "2";
+        private static final String WEST = "3";
+        private static final String EAST = "4";
+        private static final Map<Direction, String> DIRECTION_TO_COMMAND = Map.of(
+                Direction.NORTH, NORTH,
+                Direction.SOUTH, SOUTH,
+                Direction.WEST, WEST,
+                Direction.EAST, EAST
+        );
 
         private static final String WALL = "0";
         private static final String SUCCESS = "1";
 
-        private static final String NORTH = "1";
-        private static final String EAST = "4";
         private Tuple2<Integer, Integer> currentPosition = tuple(0, 0);
-        private String currentDirection = NORTH;
+        private Direction currentDirection = Direction.NORTH;
 
         @Override
         public String onReadRequested() {
-            return currentDirection;
+            return DIRECTION_TO_COMMAND.get(currentDirection);
         }
 
         @Override
         public void onStoreRequested(String result) {
             super.onStoreRequested(result);
             if (WALL.equals(result)) {
-                currentDirection = EAST;
+                currentDirection = Direction.EAST;
             }
 
             if (SUCCESS.equals(result)) {
-                currentPosition = tuple(1, 0);
+                currentPosition = currentDirection.move(currentPosition);
             }
         }
 
